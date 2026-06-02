69409295bebe7ab5f6e8a472c01cae32c5127570
isard <isard@DESKTOP-7GDQE5V>
2026-06-02 23:16:57 +0200

    test: verify all JUnit test cases pass successfully after refactoring

--- src/MagatzemTest.java
+++ src/MagatzemTest.java
@@ -7,8 +7,9 @@ class MagatzemTest {
         Article poma = new Article("Poma", 10, 20);
         Magatzem m = new Magatzem(new Article[]{poma});
         m.actualitzarEstat();
-        assertEquals(9, poma.diesPerVendre);
-        assertEquals(19, poma.qualitat);
+        
+        assertEquals(9, poma.getDiesPerVendre());
+        assertEquals(19, poma.getQualitat());
     }
 
     @Test
@@ -16,9 +17,8 @@ class MagatzemTest {
         Article formatge = new Article("Formatge Gidurat", 10, 20);
         Magatzem m = new Magatzem(new Article[]{formatge});
         m.actualitzarEstat();
-        assertEquals(9, formatge.diesPerVendre);
-        assertEquals(21, formatge.qualitat);
+        
+        assertEquals(9, formatge.getDiesPerVendre());
+        assertEquals(21, formatge.getQualitat());
     }
 }
-
-
